d1526e3be6be7b33635fc50dd526fd629482d791
Tony Rogvall <tony@rogvall.se>
2020-11-16 16:26:25 +0100

    added short cuts, 14 bit signed

--- src/i2c_ip5209.erl
+++ src/i2c_ip5209.erl
@@ -33,18 +33,28 @@ open(Bus) ->
     ok.
 
 read_voltage(Bus) ->
-    {ok,L} = i2c:smbus_read_byte_data(Bus, 16#a2),
-    {ok,H} = i2c:smbus_read_byte_data(Bus, 16#a3),
-    %%  check negative values
-    Voltage =
-	if H band 16#20 =:= 16#20 ->
-		V = ((H bor 2#1100_0000) bsl 8) + L,
-		2600.0 - V*0.26855;
-	   true ->
-		V = ((H band 16#1f) bsl 8) + L,
-		2600.0 + V*0.26855
-	end,
-    {ok, Voltage / 1000.0}.
+    L = read_byte(Bus, 16#a2),
+    H = read_byte(Bus, 16#a3),
+    <<_:2, V:14/signed>> = <<H,L>>,  %% read a2,a3 as 2block?
+    (2600.0 + abs(V)*0.26855) / 1000.0.
+
+read_intensity(Bus) ->
+    L = read_byte(Bus, 16#a4),
+    H = read_byte(Bus, 16#a5),
+    <<_:2, I:14/signed>> = <<H,L>>,  %% read a4,a5 as 2block?
+    (I*0.745985) / 1000.0.
+
+is_power_plugged_2led(Bus) ->
+    V = read_byte(Bus, 16#55),
+    V band 2#0001_0000 =/= 0.
+
+read_gpio_tap(Bus) ->
+    read_byte(Bus, 16#55).
+
+force_shutdown(Bus) ->
+    T = read_byte(Bus, 16#01),
+    T1 = T band 2#1111_1011,
+    write_byte(Bus, 16#01, T1).
 
 parse_voltage_level(Voltage) ->
     if Voltage > 0.0 ->
@@ -67,9 +77,33 @@ convert_battery_voltage_to_level(Voltage,_V0,_L0,[{V1,L1}|Curve]) ->
 convert_battery_voltage_to_level(_Voltage,_V0,_L0,[]) ->
     0.0.
 
-
-
+%% short cuts
     
+%% set (byte) register bit
+set_bits(Bus, Reg, Bits) ->
+    Byte = read_byte(Bus, Reg),
+    write_byte(Bus, Reg, Byte bor Bits).
 
-    
-    
+%% clear (byte) register bit
+clr_bits(Bus, Reg, Bits) ->
+    Byte = read_byte(Bus, Reg),
+    write_byte(Bus, Reg, Byte band (bnot Bits)).
+
+upd_bits(Bus, Reg, Set, Clr) ->
+    Byte = read_byte(Bus, Reg),
+    write_byte(Bus, Reg, (Byte bor Set) band (bnot Clr)).
+
+read_byte(Bus, Command) ->
+    {ok,Byte} = i2c:smbus_read_byte_data(Bus, Command),
+    Byte.
+
+write_byte(Bus, Command, Byte) when Byte band (bnot 16#ff) =:= 0 ->
+    {ok,_} = i2c:smbus_write_byte_data(Bus, Command, Byte),
+    ok.
+
+write_block(Bus, Command, Data) when is_binary(Data) ->
+    {ok,_} = i2c:smbus_write_i2c_block_data(Bus, Command, Data),
+    ok.
+
+read_block(Bus, Command, Len) ->
+    i2c:smbus_read_i2c_block_data(Bus, Command, Len).
