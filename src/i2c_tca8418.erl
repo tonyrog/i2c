354294ebcd9a86941a18d8327abe2131785e6235
Tony Rogvall <tony@rogvall.se>
2020-11-26 19:04:18 +0100

    using one key map, added reverse map from sym to key

--- src/i2c_tca8418.erl
+++ src/i2c_tca8418.erl
@@ -16,8 +16,8 @@
 -export([configure_3x3/1]).
 -export([configure_4x3/1]).
 -export([configure_lock/1]).
--export([keycode_3x3_to_sym/1]).
--export([keycode_4x3_to_sym/1]).
+-export([keycode_to_sym/1]).
+-export([sym_to_keycode/1]).
 
 %% default slave address
 -define(I2C_ADDR_TCA, 16#34).
@@ -141,10 +141,16 @@ open1(Bus, Addr) ->
     {ok,Port}.
 
 -define(KEYMAP_4x3,
-	#{ 1 => $1, 2 => $2, 3 => $3,
-	   11 => $4, 12 => $5, 13 => $6,
-	   21 => $7, 22 => $8, 23 => $9,
-	   31 => $*, 32 => $0, 33 => $# }).
+	#{  1  => $1, 2  => $2, 3 => $3,
+	    11 => $4, 12 => $5, 13 => $6,
+	    21 => $7, 22 => $8, 23 => $9,
+	    31 => $*, 32 => $0, 33 => $# }).
+
+-define(KEYREVMAP_4x3,
+	#{ $1 => 1,  $2 => 2,  $3 => 3,
+	   $4 => 11, $5 => 12, $6 => 13,
+	   $7 => 21, $8 => 22, $9 => 23,
+	   $* => 31, $0 => 32, $# => 33 }).
 
 configure_3x3(Bus) ->
     write_byte(Bus, ?KP_GPIO1, ?ROW0 bor ?ROW1 bor ?ROW2),
@@ -153,17 +159,19 @@ configure_3x3(Bus) ->
     write_byte(Bus, ?CFG, (?AI bor ?INT_CFG bor ?KE_IEN)),
     ok.
 
-keycode_3x3_to_sym(Key) -> maps:get(Key, ?KEYMAP_4x3).  %% same as 4x3
-
 configure_4x3(Bus) ->
     write_byte(Bus, ?KP_GPIO1, ?ROW0 bor ?ROW1 bor ?ROW2 bor ?ROW3),
     write_byte(Bus, ?KP_GPIO2, ?COL0 bor ?COL1 bor ?COL2),
     write_byte(Bus, ?KP_GPIO3, 16#00),
     write_byte(Bus, ?CFG, (?AI bor ?INT_CFG bor ?KE_IEN)),
     ok.
-	  
-keycode_4x3_to_sym(Key) -> maps:get(Key, ?KEYMAP_4x3).
 
+keycode_to_sym(Key) ->
+    maps:get(Key, ?KEYMAP_4x3).
+sym_to_keycode(Sym) ->
+    maps:get(Sym, ?KEYREVMAP_4x3).
+    
+	  
 configure_lock(Bus) ->
     configure_lock(Bus, 33, 1, 2, 10).
 
